73d315d30d932514925bb3b6c9b8b838a2c67b9c
Jesica Gutierrez <jesica.gutierrez5@soy.sena.edu.co>
2024-11-21 10:17:42 -0500

    actualizacion en el archivo gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
 const gulp = require('gulp');
+const clean = require('gulp-clean');
+const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
 
-gulp.task('build', (done) => {
-    console.log('Compilando proyecto...');
-    done();
+// Tareas de ejemplo
+gulp.task('clean', () => {
+    return gulp.src('dist', { read: false, allowEmpty: true }).pipe(clean());
 });
+
+gulp.task('styles', () => {
+    return gulp.src('src/styles/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('build', gulp.series('clean', 'styles'));
