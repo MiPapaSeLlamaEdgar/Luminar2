1b05bc7a593d0d1775a786cb80715addca3095ce
Nicolas OViedo <nicolas.oviedo707@gmail.com>
2024-11-27 22:42:01 -0500

    cambios en config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 
 const config = {
   PORT: process.env.PORT || 5000,
-  DB_HOST: process.env.DB_HOST || '127.0.0.1',
+  DB_HOST: process.env.DB_HOST || 'mysql.railway.internal',
   DB_USER: process.env.DB_USER || 'root',
-  DB_PASSWORD: process.env.DB_PASSWORD || '',
+  DB_PASSWORD: process.env.DB_PASSWORD || 'gSJNclRyjCkRjaPtWHyFKbZyOAfsmtkw',
   DB_NAME: process.env.DB_NAME || 'Luminar',
   DB_PORT: process.env.DB_PORT || 3306,
   JWT_SECRET: process.env.JWT_SECRET || 'supersecretkey',
