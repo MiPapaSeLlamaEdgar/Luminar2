eedcd86a4060d30ac7978571e1d30ccfa81d8a76
Nicolas OViedo <nicolas.oviedo707@gmail.com>
2024-11-28 00:15:09 -0500

    cambios en server.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const config = {
   PORT: process.env.PORT || 5000,
-  DB_HOST: process.env.DB_HOST || 'mysql.railway.internal',
+  DB_HOST: process.env.DB_HOST || '127.0.0.1',
   DB_USER: process.env.DB_USER || 'root',
   DB_PASSWORD: process.env.DB_PASSWORD || 'gSJNclRyjCkRjaPtWHyFKbZyOAfsmtkw',
   DB_NAME: process.env.DB_NAME || 'railway',
