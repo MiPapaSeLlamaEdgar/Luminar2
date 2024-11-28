202db2d1930b8524b9531f31d8749584d2809c40
Nicolas OViedo <nicolas.oviedo707@gmail.com>
2024-11-27 23:25:24 -0500

    cambios en server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
-    logging: process.env.NODE_ENV === 'development' ? console.log : false,
+    logging: false,
     pool: {
         max: 5,
         min: 0,
